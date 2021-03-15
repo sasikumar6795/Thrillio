3a10043a8c232c95ef5ca72e2aa3d45558358832
sasikumar6795 <32613154+sasikumar6795@users.noreply.github.com>
2021-03-15 20:28:42 +0530

    Streams demo updated

---
+++ Practice/src/com/sasi/functional/streams/StreamsDemo.java
@@ -0,0 +1,79 @@
+package com.sasi.functional.streams;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.htmlcleaner.HtmlCleaner;
+import org.w3c.dom.stylesheets.DocumentStyle;
+
+public class StreamsDemo {
+
+    static String doc1 = "<html><body>One of the most common uses of <i>streams</i> is to represent queries over data in collections</body></html>";
+    static String doc2 = "<html><body>Information integration systems provide valuable services to users by integrating information from a number of autonomous, heterogeneous and distributed Web sources</body></html>";
+    static String doc3 = "<html><body>Solr is the popular, blazing fast open source enterprise search platform from the Apache Lucene</body></html>";
+    static String doc4 = "<html><body>Java 8 goes one more step ahead and has developed a streams API which lets us think about parallelism</body></html>";
+
+    static List<String> documents = new ArrayList<>(Arrays.asList(doc1, doc2, doc3, doc4));
+    
+    
+    public static void declarative() {
+	System.out.println("\n\nDeclarative: \n");
+	/*
+	// Stream pipeline (a common structure): 
+	     (a) set-up stream source (~ tables in SQL world)
+	     (b) 0 or more intermediate operations (~ WHERE clause) -- lazy 
+	              & return Stream<T>, i.e., transforms a stream into another stream
+	     (c) terminal operation (~ column names) -- eager 
+	              & return NON-STREAM. Terminates (closes) a stream
+	*/		
+	
+	documents.stream()
+	.filter(d -> d.contains("stream"))
+	.map(Indexer::stripHtmlTags)
+	.map(Indexer::removeStopwords)
+	.forEach(System.out::println);
+	
+    }
+
+
+    public static void main(String[] args) {
+
+	declarative();
+
+    }
+    
+    
+
+   
+}
+
+class Indexer {
+	
+	private static List<String> stopWords = Arrays.asList("of", "the", "a", "is", "to", "in", "and");
+	
+	static String stripHtmlTags(String doc) {
+		return new HtmlCleaner().clean(doc).getText().toString();
+	}
+	
+	static String removeStopwords(String doc) {
+		
+		StringBuilder sb = new StringBuilder();
+		/*
+		 * for (String word : doc.split(" ")) { if (!stopWords.contains(word))
+		 * sb.append(word).append(" "); }
+		 */
+		
+		String[] split = doc.split(" ");
+		for (int i = 0; i < split.length; i++) {
+		    if(!stopWords.contains(split[i]))
+		    {
+			sb.append(split[i]).append(" ");
+			
+		    }
+		}
+		
+		return sb.toString();
+	}	
+	
+}
